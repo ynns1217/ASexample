89056c09c2fdebaede00a02f2f4953f55cf78258
ynns1217 <ynns1217@naver.com>
2022-04-20 11:57:03 +0900

    7 장

---
+++ 7장/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//p.232 7장 클래스
+
+using System;
+
+class Global
+{
+    public static int Count = 0;
+}
+
+class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void 1Main()
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}
+
---
+++ 7장/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+	class MainApp
+
+	{
+		//프로그램 실행이 시작되는 곳
+		static void 1Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("사용법 : Hello.exe <이름>");
+				return;
+			}
+			WriteLine("Hello,{0}!", args[0]);      //프롬프트에 출력
+		}
+	}
+}
