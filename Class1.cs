e9a6a6778a66541d5cef12bd7d7f8ddccf10cc35
ynns1217 <ynns1217@naver.com>
2022-04-20 11:33:19 +0900

    dd

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//p.232 7장 클래스
 
-public class Class1
+using System;
+
+class Global
 {
-	public Class1()
-	{
-	}
+    public static int Count = 0;
 }
+
+class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}");
+
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+
+        Console.WriteLine($"Global.Count : {Global.Count}");
+    }
+}
+
